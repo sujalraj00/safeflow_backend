03ab184a2d85979cda6b5f7e13cfe37e3b4a5f97
Sujal Raj <sujalraj611@gmail.com>
2025-05-04 09:14:26 +0530

    error resolved for deployment

--- routes/sosRoutes.js
+++ routes/sosRoutes.js
@@ -1,27 +1,27 @@
-// routes/sosRoutes.js
-const express = require('express');
-const router = express.Router();
-const admin = require('../firebase/firebaseConfig');
-const Token = require('../models/Token');
+// // routes/sosRoutes.js
+// const express = require('express');
+// const router = express.Router();
+// //const admin = require('../firebase/firebaseConfig');
+// const Token = require('../models/Token');
 
-router.post('/trigger-sos', async (req, res) => {
-  try {
-    const userTokens = await Token.find({ userType: 'user' });
+// router.post('/trigger-sos', async (req, res) => {
+//   try {
+//     const userTokens = await Token.find({ userType: 'user' });
 
-    const messages = userTokens.map((user) => ({
-      token: user.token,
-      notification: {
-        title: '🚨 SOS Alert!',
-        body: 'Flood risk detected nearby. Take immediate precautions.',
-      },
-    }));
+//     const messages = userTokens.map((user) => ({
+//       token: user.token,
+//       notification: {
+//         title: '🚨 SOS Alert!',
+//         body: 'Flood risk detected nearby. Take immediate precautions.',
+//       },
+//     }));
 
-    const response = await admin.messaging().sendEach(messages);
-    res.json({ success: true, response });
-  } catch (err) {
-    console.error('FCM Error:', err);
-    res.status(500).json({ error: 'Failed to send SOS alert' });
-  }
-});
+//     const response = await admin.messaging().sendEach(messages);
+//     res.json({ success: true, response });
+//   } catch (err) {
+//     console.error('FCM Error:', err);
+//     res.status(500).json({ error: 'Failed to send SOS alert' });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
